1e321e1102707d31ddf4c87a16e2fad9c861c8e6
Mafikul Islam <mdmafikulislam5320@gmail.com>
2025-07-26 20:14:31 +0600

    all Payment system by strip

--- src/Pages/PaymentSystem/CheckoutForm.jsx
+++ src/Pages/PaymentSystem/CheckoutForm.jsx
@@ -1,9 +1,12 @@
 import { CardElement, useElements, useStripe } from '@stripe/react-stripe-js';
+import { LockKeyhole } from 'lucide-react';
 import React from 'react';
+import useAxiosSucure from '../../Hooks/useAxiosSucure';
 
-const CheckoutForm = () => {
+const CheckoutForm = ({ data }) => {
     const stripe = useStripe();
     const elements = useElements();
+    const axiosSucure = useAxiosSucure()
 
     const handleSubmit = async (e) => {
         e.preventDefault();
@@ -26,30 +29,54 @@ const CheckoutForm = () => {
         } else {
             console.log('[PaymentMethod]', paymentMethod);
         }
+        const res = await axiosSucure.post("/create-payment-intent", {
+            amount: data?.price * 100
+        })
+        console.log(res.data.clientSecret)
     }
     return (
-        <div>
-            <form onSubmit={handleSubmit}>
-                <CardElement
-                    options={{
-                        style: {
-                            base: {
-                                fontSize: '16px',
-                                color: '#424770',
-                                '::placeholder': {
-                                    color: '#aab7c4',
+        <div className="mt-10">
+            <div className="bg-gradient-to-br from-amber-50 to-orange-50 border border-amber-200 shadow-xl rounded-2xl p-8">
+                <h2 className="text-2xl font-bold text-amber-600 mb-4 text-center">
+                    {data?.name}
+                </h2>
+                <p className="text-sm text-slate-600 mb-6 text-center">
+                    {'Full access to premium news.'}
+                </p>
+
+                <form onSubmit={handleSubmit} className="space-y-6">
+                    <div className="p-4 border border-amber-300 rounded-xl bg-white shadow-sm">
+                        <CardElement
+                            options={{
+                                style: {
+                                    base: {
+                                        fontSize: '16px',
+                                        color: '#1e293b',
+                                        '::placeholder': {
+                                            color: '#cbd5e1',
+                                        },
+                                    },
+                                    invalid: {
+                                        color: '#dc2626',
+                                    },
                                 },
-                            },
-                            invalid: {
-                                color: '#9e2146',
-                            },
-                        },
-                    }}
-                />
-                <button className='btn' type="submit" disabled={!stripe}>
-                    Pay
-                </button>
-            </form>
+                            }}
+                        />
+                    </div>
+
+                    <button
+                        type="submit"
+                        disabled={!stripe}
+                        className="w-full items-center py-3 rounded-xl bg-gradient-to-r from-amber-500 to-amber-600 text-white font-semibold hover:opacity-90 transition duration-200"
+                    >
+                        Pay {data?.price}
+                    </button>
+                </form>
+
+                <div className="mt-6 flex justify-center font-bold items-center gap-2 text-center text-sm text-amber-500">
+                    <LockKeyhole size={15} /> Secured by Stripe
+                </div>
+            </div>
         </div>
     );
 };
--- src/Pages/PaymentSystem/PaymentPage.jsx
+++ src/Pages/PaymentSystem/PaymentPage.jsx
@@ -8,29 +8,32 @@ import { useQuery } from '@tanstack/react-query';
 import useAuth from '../../Hooks/useAuth';
 import useAxiosSucure from '../../Hooks/useAxiosSucure';
 import { useParams } from 'react-router';
-const stripePromise = loadStripe('pk_test_6pRNASCoBOKtIshFeQd4XMUh');
+const stripePromise = loadStripe(import.meta.env.VITE_payment_key);
 const PaymentPage = () => {
   const { User } = useAuth();
   const axiosSucure = useAxiosSucure();
   const params = useParams()
+  console.log(params.id)
+  console.log(User?.email)
   const { data } = useQuery({
     queryKey: ["plan", User?.email],
-    queryFn: () => {
-      const res = axiosSucure.get(`/subscription?id=${params.id}`)
+    queryFn: async () => {
+      const res = await axiosSucure.get(`/payment?id=${params.id}`)
       return res.data
     }
   })
+  console.log(data)
   return (
     <div className="min-h-screen bg-gray-50 flex items-center justify-center">
       <div className="w-full max-w-6xl bg-white shadow-xl rounded-2xl flex overflow-hidden">
         {/* Left Side */}
-        <div className="w-1/2 p-10 space-y-6 border-r">
+        <div className="w-1/2 p-10 space-y-6 border-amber-200 border-r-2">
           <div className="flex items-center gap-2">
             <div className="w-6 h-6 rounded-full bg-black flex items-center justify-center text-white text-sm font-bold">K</div>
-            <span className="text-sm font-medium">Kavholm</span>
+            <span className="text-sm font-medium">{User?.displayName}</span>
           </div>
-          <h2 className="text-gray-700 text-lg">Touchwood Counter Stool</h2>
-          <h1 className="text-3xl font-bold">$71.21</h1>
+          <h2 className="text-gray-700 text-lg font-bold">{data?.name}</h2>
+          <h1 className="text-3xl font-bold">${data?.price}</h1>
           <div className="">
             <Lottie animationData={PaymentAnimation} loop={true} />
           </div>
@@ -39,7 +42,7 @@ const PaymentPage = () => {
         {/* Right Side */}
         <div className='w-1/2 p-10'>
           <Elements stripe={stripePromise}>
-            <CheckoutForm></CheckoutForm>
+            <CheckoutForm data={data}></CheckoutForm>
           </Elements>
         </div>
       </div>
